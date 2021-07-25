08d3bb2630573f66544a6dc7493ca4ca867acb6f
Artur (antimech) <arthur.antimech@pm.me>
2021-07-25 20:22:53 +0600

    Create templates migration

---
+++ migrations/m210725_070847_create_templates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%templates}}`.
+ */
+class m210725_070847_create_templates_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%templates}}', [
+            'id' => $this->bigPrimaryKey(),
+            'user_id' => $this->bigInteger()->notNull(),
+            'FOREIGN KEY(user_id) REFERENCES users(id)',
+            'text' => $this->text(),
+            'created_at' => $this->timestamp(),
+            'updated_at' => $this->timestamp(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%templates}}');
+    }
+}
