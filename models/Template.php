232ad3814dbf67fe407fade238fa81603d31ac08
Artur (antimech) <arthur.antimech@pm.me>
2021-07-25 20:24:47 +0600

    Generate models from migrations

---
+++ models/Template.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "templates".
+ *
+ * @property int $id
+ * @property string|null $text
+ * @property string $created_at
+ * @property string|null $updated_at
+ *
+ * @property Queue[] $queues
+ */
+class Template extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'templates';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['text'], 'string'],
+            [['created_at'], 'required'],
+            [['created_at', 'updated_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'text' => 'Text',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[Queues]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getQueues()
+    {
+        return $this->hasMany(Queue::className(), ['template_id' => 'id']);
+    }
+}
